570f23f18f4db1931428aa119e108359d9ae779a
L-pop752 <leslove0804@gmail.com>
2026-07-14 04:26:17 -0500

    Update biblioteca.sql
    cree la base de datos

--- biblioteca.sql
+++ biblioteca.sql
@@ -21,6 +21,9 @@ SET time_zone = "+00:00";
 -- Base de datos: `biblioteca`
 --
 
+CREATE DATABASE IF NOT EXISTS biblioteca;
+USE biblioteca;
+
 -- --------------------------------------------------------
 
 --
